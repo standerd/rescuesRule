363bd0c4ad522ea49e3495b92dc876ae16111648
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2019-10-22 14:33:20 +0200

    added helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {Helmet} from "react-helmet"
 import Landing from "../src/landingPage/landing";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+    <title>Rescues Rule</title>
+    <meta name="RescuesRule" content="Help Support the Rescues" />
+  </Helmet>
       <Landing />
     </div>
   );
